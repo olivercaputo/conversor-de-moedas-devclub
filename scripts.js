146889d0a75f655f721f6de68376ebcdf9d8b3ea
Oliver Caputo <olivercaputo@gmail.com>
2022-03-25 16:08:49 -0300

    commit start convert values dinamic

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 const button = document.getElementById("convert-button")
 const select = document.getElementById("current-select")
-const dolar = 5.1
-const euro = 5.71
+
+const dolar = 5.1;
+const euro = 5.71;
 const bitcoin = 196755.62
-// 0,0000051
+// https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL
 
 const convertValues = () => {
     const inputReais = document.getElementById("input-real").value
     const realValueText = document.getElementById("real-value-text")
     const currencyValueText = document.getElementById("currency-value-text")
+
+    const data = fetch ("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    console.log(data)
 
     realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
